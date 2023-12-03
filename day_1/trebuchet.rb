104790ecdb18acd80444c6f6bb7d5c9f7460cd7a
Kirsten Comandich <kcomandich@gmail.com>
2023-12-03 14:40:49 -0800

    Day 1 - Trebuchet: rough solution that solves part one

--- day_1/trebuchet.rb
+++ day_1/trebuchet.rb
@@ -1,14 +1,24 @@
 require 'pry-byebug'
 
 class Trebuchet
+  attr_accessor :digits
 
   def initialize
      @input = IO.read("input.txt").split("\n")
+     @digits = []
+  end
+
+  def extract_digits
+     @input.each do |i|
+       @digits.push("#{i.match(/\d/)[0]}#{i.reverse.match(/\d/)[0]}".to_i)
+     end
   end
 end
 
 if $PROGRAM_NAME == __FILE__
   t = Trebuchet.new
 
-  puts "Solution"
+  t.extract_digits
+
+  puts "Solution: #{t.digits.sum}"
 end
