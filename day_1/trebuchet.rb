6844e9a6dd42343c2f7940e73ef474d5867a3d90
Kirsten Comandich <kcomandich@gmail.com>
2023-12-08 12:10:30 -0800

    Day 1 - Trebuchet: Solution for part 2
    Was stuck for a while - had to scan the string for all matches, including ones that overlap!

--- day_1/trebuchet.rb
+++ day_1/trebuchet.rb
@@ -6,12 +6,37 @@ class Trebuchet
   def initialize
      @input = IO.read("input.txt").split("\n")
      @digits = []
+     @words = %w(zero one two three four five six seven eight nine)
   end
 
   def extract_digits
      @input.each do |i|
-       @digits.push("#{i.match(/\d/)[0]}#{i.reverse.match(/\d/)[0]}".to_i)
+       @digits.push("#{first_digit(i)}#{last_digit(i)}".to_i)
      end
+  end
+
+  private
+
+  def all_digits(string)
+    # use (?=pat) so that overlapping words are all captured
+    # for example "three28jxdmlqfmc619eightwol" => ["three", "2", "8", "6", "1", "9", "eight", "two"]
+    # instead of => ["three", "2", "8", "6", "1", "9", "eight"]
+    # Positive lookahead assertion: ensures that the following characters match pat,
+    # but doesn’t include those characters in the matched text
+    # https://ruby-doc.org/3.2.2/Regexp.html
+    string.scan(/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/).flatten
+  end
+
+  def first_digit(string)
+    num = all_digits(string)[0]
+
+    return @words.index(num) ?  @words.index(num) : num
+  end
+
+  def last_digit(string)
+    num = all_digits(string)[-1]
+
+    return @words.index(num) ?  @words.index(num) : num
   end
 end
 
