69e00761e0f96dbec1301c2b21de2236521ad3dc
Kirsten Comandich <kcomandich@gmail.com>
2023-12-08 20:11:38 -0800

    Day 2 - Cube Conundrum: rough solution to part two

--- day_2/cube_conundrum.rb
+++ day_2/cube_conundrum.rb
@@ -5,6 +5,7 @@ class CubeConundrum
     @input = IO.read("input.txt").lines
     @games = []
     @valid_games = []
+    @min_game_colours = []
     @colours = {'red' => 12, 'green' => 13, 'blue' => 14}
   end
 
@@ -23,6 +24,28 @@ class CubeConundrum
 
     @valid_games.sum
   end
+
+  def find_fewest_colour_count
+    @input.each_with_index do |line, i|
+      @games[i] = line.match(/Game \d+: (.*)/)[1].split(';')
+
+      @min_game_colours[i] = {}
+      @colours.keys.each do |c|
+        @min_game_colours[i][c] = 0
+      end
+
+      @games[i].each_with_index do |round, r|
+        @colours.keys.each do |c|
+          num = @games[i][r].match(/(\d+) #{c}/)
+          @min_game_colours[i][c] = num[1].to_i if num && num[1].to_i > @min_game_colours[i][c]
+        end
+      end
+    end
+
+    @min_game_colours.map do |g|
+      g.values.inject(:*)
+    end.sum
+  end
 end
 
 if $PROGRAM_NAME == __FILE__
@@ -30,4 +53,5 @@ if $PROGRAM_NAME == __FILE__
 
   puts "Solution"
   puts cc.find_invalid_games
+  puts cc.find_fewest_colour_count
 end
